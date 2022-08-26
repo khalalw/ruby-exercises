0103efbdfa6881da7aac3624a3bddd03b3fd22bf
Khalal Walker <khalalw@outlook.com>
2022-08-26 15:28:19 -0700

    finish nested has exercises

--- ruby_basics/12_nested_collections/exercises/nested_hash_exercises.rb
+++ ruby_basics/12_nested_collections/exercises/nested_hash_exercises.rb
@@ -1,3 +1,5 @@
+# @param [Hash<Symbol, Hash>] languages
+# @param [Symbol] language_name
 def find_language_information(languages, language_name)
   # Take languages (a nested hash) and language_name as a symbol, return the
   # value for the language_name key (which will be another hash!)
@@ -7,33 +9,59 @@ def find_language_information(languages, language_name)
   #   ruby: { initial_release: 'December 25, 1996', is_beautiful?: true },
   #   javascript: { initial_release: 'December 4, 1995', is_beautiful?: false }
   # }
+  languages[language_name]
 end
 
+# @param [Hash<Symbol, Hash>] languages
+# @param [Symbol] language_name
+# @param [Symbol] info_key
+# @param [Object] info_value
 def add_information_about_language(languages, language_name, info_key, info_value)
   # Take languages and add the key/value pair info_key/info_value to the nested
   # hash of language_name, then return the updated languages hash
+  languages[language_name][info_key] = info_value
+  languages
 end
 
+# @param [Hash<Symbol, Hash>] languages
+# @param [Symbol] language_name
+# @param [Hash] language_info_value
 def add_language(languages, language_name, language_info_value)
   # Take languages and add the key/value pair language_name/language_info_value
   # to it, then return languages
+  languages[language_name] = language_info_value
+  languages
 end
 
+# @param [Hash<Symbol, Hash>] languages
+# @param [Symbol] language_name
+# @param [Hash] info_key
 def delete_information_about_language(languages, language_name, info_key)
   # Take languages and delete the key/value pair with key info_key from
   # language_name, then return languages
+  languages[language_name].delete(info_key)
+  languages
 end
 
+# @param [Hash<Symbol, Hash>] languages
+# @param [Symbol] language_name
 def delete_language(languages, language_name)
   # Take languages and delete the language_name key/value pair, then return
   # languages
+  languages.delete(language_name)
+  languages
 end
 
+# @param [Hash<Symbol, Hash>] languages
 def find_beautiful_languages(languages)
   # Take languages and return a hash containing only languages which have the
   # key/value pair { is_beautiful?: true } listed in their information
+  languages.select { |_, val| val[:is_beautiful?] }
 end
 
+# @param [Hash<Symbol, Hash>] languages
+# @param [Symbol] language_name
+# @param [Integer] fact_index
 def find_language_facts(languages, language_name, fact_index = 0)
   # Take languages (now with additional facts added to each language with the
   # key :facts and value of an array of strings) and return the fact
@@ -50,4 +78,5 @@ def find_language_facts(languages, language_name, fact_index = 0)
   #                 initial_release: 'December 4, 1995',
   #                 is_beautiful?: false }
   # }
+  languages[language_name][:facts][fact_index]
 end
