a328938f82a9c7438179f6bba4c8c9b753fd361e
Khalal Walker <khalalw@outlook.com>
2022-08-27 08:07:58 -0700

    remove pointless describe

--- ruby_problems/caesar_cipher/spec/caesar_cipher_spec.rb
+++ ruby_problems/caesar_cipher/spec/caesar_cipher_spec.rb
@@ -2,10 +2,8 @@ require 'spec_helper'
 require_relative '../caesar_cipher'
 
 RSpec.describe 'Caesar Cipher' do
-  describe 'caesar shift' do
-    it 'handles the shift' do
-      solution = Solution.new
-      expect(solution.caesar_cipher("What a string!", 5)).to eq('Bmfy f xywnsl!')
-    end
+  it 'handles the shift' do
+    solution = Solution.new
+    expect(solution.caesar_cipher("What a string!", 5)).to eq('Bmfy f xywnsl!')
   end
 end
